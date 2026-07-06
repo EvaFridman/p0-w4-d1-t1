2b3260fe2640f1251f9c7185f2d6ae4568b553b2
Eva Fridman <fridman.1999@mail.ru>
2026-07-06 12:23:05 +0300

    release 2

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const pawnBlack = "♟";
 const pawnWhite = "♙";
 const chessBoard = document.querySelector("#board");
 
-const cells = Array.from({length: 64}, (v, i) => {
+const cells = Array.from({length: 64}, (_, i) => {
     const p = document.createElement("p");
     p.classList.add("cell");
     p.dataset.index = i;
-    if ((p.dataset.index) % 2) {
-        p.classList.add("light");
-    } else {
-        p.classList.add("dark");
-    }
     return p;
 });
 
 chessBoard.append(...cells);
+
+cells.forEach((p, i) => p.classList.add((Math.floor(i / 8) + i) % 2 ? "dark" : "light"));
+
+cells.forEach(p => Number(p.dataset.index) >= 0 && Number(p.dataset.index) < 8 && (p.textContent = backRankBlack[p.dataset.index-0]));
+cells.forEach(p => Number(p.dataset.index) >= 8 && Number(p.dataset.index) < 16 && (p.textContent = pawnBlack));
+cells.forEach(p => Number(p.dataset.index) >= 48 && Number(p.dataset.index) < 56 && (p.textContent = pawnWhite));
+cells.forEach(p => Number(p.dataset.index) >= 56 && Number(p.dataset.index) < 64 && (p.textContent = backRankWhite[p.dataset.index-56]));
